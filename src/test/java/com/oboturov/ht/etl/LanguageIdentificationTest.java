0c117b82127a94bddc4e59efb882e8e1d562a283
Artem Oboturov <oboturov@gmail.com>
2012-11-25 15:53:12 +0100

    LanguageIdentification implement language detection and filtering out of non-english phrases.

---
+++ src/test/java/com/oboturov/ht/etl/LanguageIdentificationTest.java
@@ -0,0 +1,24 @@
+package com.oboturov.ht.etl;
+
+import org.apache.tika.language.LanguageIdentifier;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
+
+/**
+ * @author aoboturov
+ */
+public class LanguageIdentificationTest {
+
+    @Test
+    public void loads_languages_from_tika_library() throws IOException {
+        final Set<String> languages = LanguageIdentifier.getSupportedLanguages();
+        assertNotNull(languages, "A set of supported languages required");
+        assertFalse(languages.isEmpty(), "Some languages must be supported");
+    }
+
+}
